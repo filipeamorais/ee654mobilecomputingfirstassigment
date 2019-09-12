f1acf4656e8866cd40d14047bf1a437b50eac989
filipeamorais@pm.me <filipeamorais@pm.me>
2019-09-11 23:32:11 -0500

    add: added the function to make the button give the result on a textView

--- app/src/main/java/com/example/firstassigment/MainActivity.java
+++ app/src/main/java/com/example/firstassigment/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.firstassigment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void addOp(View view){
+        EditText editText =
+                (EditText) findViewById(R.id.edittext1);
+        String message = editText.getText().toString();
+        TextView textView =
+                (TextView) findViewById(R.id.textview1);
+        textView.setTextSize(40);
+        textView.setText(message);
+    }
+
+
 }
